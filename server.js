68b6b86654352252b8deaabb974410c32e98914d
ArpitBansal3006 <arpitbansal717@gmail.com>
2025-06-20 10:31:29 +0530

    Cors

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express=require('express');//expres module included 
+const cookieParser=require('cookie-parser');
+const cors=require('cors');
+const authRoutes=require('./src/routes/authRoutes');
 
-const authRoutes=require('./src/routes/authRoutes')
 
 const app=express();//instanceof express application
 
 app.use(express.json());//middleware
+app.use(cookieParser());
+const corsOptions={
+    origin:'http://localhost:3000',
+    credentials:true
+};
+app.use(cors(corsOptions));
+
 app.use('/auth',authRoutes);
 
 const PORT=8001;
