90643569f4fdf1d24761c010cde538dd54fe5150
ArpitBansal3006 <arpitbansal717@gmail.com>
2025-06-26 09:50:11 +0530

    Database Integration

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express=require('express');//expres module included
 const cookieParser=require('cookie-parser');
 const cors=require('cors');
 const authRoutes=require('./src/routes/authRoutes');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Database connected'))
+.catch(error => console.log(error));
 const app=express();//instanceof express application
 
 app.use(express.json());//middleware
 app.use(cookieParser());
 const corsOptions={
-    origin:'http://localhost:3000',
+    origin:' process.env.CLIENT_ENDPOINT',
     credentials:true
 };
 app.use(cors(corsOptions));
--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,15 +1,37 @@
 const jwt =require('jsonwebtoken');
 const secret="01978eda-4942-4fc4-b7ed-924be5321f95";
+const bcrypt = require('bcryptjs');
+const Users = require('../model/Users');
 
 const authController={
-    login:(request, response)=>{
+    login: async (request, response) => {
+        try{
         // those values are here because of express.json() middleware
         const{username,password}=request.body;
+        const data = await Users.findOne({ email:username });
+        if (!data) {
+return response.status(401).json({
+message: 'Invalid Credentials' });
+
+ }
+
+const isMatch = await
+bcrypt.compare(password, data.password);
+
+if (!isMatch) {
+ return response.status(401).json({
+message: 'Invalid Credentials' });
+
+ }
+
         
-        if(username==='admin' && password==='admin'){
             const userDetails={
-                name:"Arpit",
-                email:"arpit@b.com"
+                id: data._id,
+                name: data.name,
+                email: data.email
+
+
+                
             };
             const token=jwt.sign(userDetails,secret,{expiresIn:'1h'});
             response.cookie('jwtToken',token,{//key and value and configuration
@@ -22,8 +44,10 @@ const authController={
             response.json({message:'User authenticated',userDetails: userDetails});
 
         }
-        else{
-            response.status(401).json({message:'Invalid credentials'});
+       catch (error) {
+        console.log(error);
+
+            response.status(500).json({ error:'Internal server error ' });
         }
 
     },
